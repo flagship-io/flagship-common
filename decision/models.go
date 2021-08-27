dad5b52dd9d1b954d2779d909f5478d119b17d05
Kévin JOSE <kevin.jose.92@gmail.com>
2021-08-27 16:38:42 +0200

    add targeting and allocation

---
+++ decision/models.go
@@ -0,0 +1,19 @@
+package decision
+
+import (
+	"github.com/flagship-io/flagship-proto/targeting"
+)
+
+// VariationInfo stores the variation information for decision making
+type Variation struct {
+	ID         string
+	Allocation float32
+	Traffic    float32
+}
+
+// VariationsGroupInfo stores the variation group information for decision making
+type VariationsGroup struct {
+	ID         string
+	Targetings *targeting.Targeting
+	Variations []*Variation
+}
